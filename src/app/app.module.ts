d14b4a99278d9f3b77e42eed5562867c5aefb73c
AgustinFrich <agustinfrich@gmail.com>
2025-07-23 00:27:22 -0300

    update a angular 15 sencillo y pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -16,6 +16,8 @@ import { TiempoPipePipe } from './pipes/tiempo-pipe.pipe';
 import { FormsModule } from '@angular/forms';
 import { AgregarCancionComponent } from './components/agregar-cancion/agregar-cancion.component';
 import { NavegacionComponent } from './components/navegacion/navegacion.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { RouterOutlet } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -30,10 +32,17 @@ import { NavegacionComponent } from './components/navegacion/navegacion.componen
     BrowserModule,
     FormsModule,
     AppRoutingModule,
+    RouterOutlet,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
     provideStorage(() => getStorage()),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
   providers: [{ provide: FIREBASE_OPTIONS, useValue: environment.firebase }],
   bootstrap: [AppComponent],
